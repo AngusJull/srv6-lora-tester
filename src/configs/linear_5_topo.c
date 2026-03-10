f48af0e5604e6708cb7aa1c08990c2402903108b
Angus Jull <angusjull1@gmail.com>
2026-03-10 16:08:05 -0400

    Fix forwarding entries for linear 5 topo

--- src/configs/linear_5_topo.c
+++ src/configs/linear_5_topo.c
@@ -13,16 +13,25 @@ static struct srv6_route _routes[] = {
 };
 
 static struct forwarding_entry _fib[] = {
-    // 0 -> 1 -> 2 -> 3 -> 4
     { .install_id = 0, .dest_id = 4, .next_hop_id = 1 },
-    { .install_id = 1, .dest_id = 4, .next_hop_id = 2 },
-    { .install_id = 2, .dest_id = 4, .next_hop_id = 3 },
-    { .install_id = 3, .dest_id = 4, .next_hop_id = 4 },
-    // 4 -> 3 -> 2 -> 1 -> 0
-    { .install_id = 4, .dest_id = 0, .next_hop_id = 3 },
-    { .install_id = 3, .dest_id = 0, .next_hop_id = 2 },
-    { .install_id = 2, .dest_id = 0, .next_hop_id = 1 },
+    { .install_id = 0, .dest_id = 1, .next_hop_id = 1 },
+
     { .install_id = 1, .dest_id = 0, .next_hop_id = 0 },
+    { .install_id = 1, .dest_id = 2, .next_hop_id = 2 },
+    { .install_id = 1, .dest_id = 4, .next_hop_id = 2 },
+
+    { .install_id = 2, .dest_id = 4, .next_hop_id = 3 },
+    { .install_id = 2, .dest_id = 0, .next_hop_id = 1 },
+    { .install_id = 2, .dest_id = 1, .next_hop_id = 1 },
+    { .install_id = 2, .dest_id = 3, .next_hop_id = 3 },
+
+    // 4 -> 3 -> 2 -> 1 -> 0
+    { .install_id = 3, .dest_id = 4, .next_hop_id = 4 },
+    { .install_id = 3, .dest_id = 0, .next_hop_id = 2 },
+    { .install_id = 3, .dest_id = 2, .next_hop_id = 2 },
+
+    { .install_id = 4, .dest_id = 0, .next_hop_id = 3 },
+    { .install_id = 4, .dest_id = 3, .next_hop_id = 3 },
 };
 
 static struct address_configuration addr_config[NUM_NODES] = {
